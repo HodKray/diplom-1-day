9edad70c62f4828231384ad2847dff65385a8a59
Tolia <anatoli-peshcov@mail.ru>
2021-05-07 15:55:29 +0300

    header

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -3,8 +3,8 @@
 @section('title','Главная')
 
 @section('content')
-    <div class="container">
-        <h2 class="text-center">Новые <span>товары</span></h2>
+    <div class="container ">
+        <h2 class="text-center my-3">Новые <span>товары</span></h2>
         <div class="row">
             @foreach($new_products as $product)
                 @include('components.product-card', $product)
